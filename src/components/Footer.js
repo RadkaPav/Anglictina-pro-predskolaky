f1d1ed09be65886e5a1143d79e1e222df7a7b061
Radka Pavlišová <radojka@seznam.cz>
2024-05-14 15:44:07 +0200

    Nainstalován Tailwind
    a nastylovány Header.js a Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import image from "../img-sound/kids.png"
 
 const Footer = () => {
+  const [isHidden, setIsHidden] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsHidden(true)
+    }, 2000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
-    <img src={image} alt='' className='kids' />
+    <div className={`w-full  absolute bottom-0 bg-cover bg-center ${isHidden ? "hidden" : ""}`} >
+      <img src={image} alt='' className='mx-auto h-[80vh]' />
+    </div>
+
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const Header = () => {
+
+  const [isHidden, setIsHidden] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsHidden(true)
+    }, 2000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
-    <div><h1>Angličtina pro předškoláky</h1></div>
+    <div><h1 className={`text-3xl font-bold uppercase pt-10 text-center ${isHidden ? "hidden" : ""}`}>Angličtina pro předškoláky</h1></div>
   )
 }
 
